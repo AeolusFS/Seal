a96a25abd2c8eabad9c338b33488a3e79bb33df7
AeolusFS <zhai@ualberta.ca>
2020-02-02 10:49:38 +0800

    ci collision

--- Seals/Script/Player.gd
+++ Seals/Script/Player.gd
@@ -4,17 +4,17 @@ var motion = Vector2()
 var gravity = 12
 var acceleration = 40
 var MAX_speed = 200
-var MAX_gravity = 500
-var MAX_bounce = 1000
+var MAX_gravity = 300
 var jump = Vector2(0, -1)
-var jump_height = -300
+var jump_height = -200
 var on_wall_speed = 80
 var on_wall_l = false
 var on_wall_r = false
-var bounce_height = 500
-var bounce_width = 1600
+var bounce_height = 350
 onready var can_double_jump = 1
 onready var player_anim = get_node("Player_sprite")
+
+onready var tem_start_pos = Vector2(1582, 1310)
 
 func _physics_process(delta):
 	
@@ -47,28 +47,39 @@ func _physics_process(delta):
 	if is_on_floor():
 		can_double_jump = 1
 		if Input.is_action_just_pressed("ui_up"):
+			if Input.is_action_pressed("ui_left"):
+				motion.x = max(motion.x - acceleration, -MAX_speed)
+			if Input.is_action_pressed("ui_right"):
+				motion.x = min(motion.x + acceleration, MAX_speed)
 			motion.y = jump_height
 	else:
 		if Input.is_action_just_pressed("ui_up") and can_double_jump == 1:
 			motion.y = jump_height
 			can_double_jump = 0
-		else:
-			motion.y = min(motion.y + gravity, MAX_gravity)
+		motion.y = min(motion.y + gravity, MAX_gravity)
+
 
 	move_and_slide(motion*delta*70, jump)
 	
 	if on_wall_l:
-		if motion.x > -200:
-			motion.x -= 200
-		if motion.x < -200:
+		if motion.x > -150:
+			motion.x -= 150
+		if motion.x < -150:
 			on_wall_l = false
 	
 	if on_wall_r:
-		if motion.x < 200:
-			motion.x += 200
-		if motion.x > 200:
+		if motion.x < 150:
+			motion.x += 150
+		if motion.x > 150:
 			on_wall_r = false
 			
+	var collision = move_and_collide(motion * delta)
+	if collision:
+		#velocity = motion.bounce(collision.normal)
+		if collision.collider.get_name() == "OuterCier" or collision.collider.get_name() == "InnerCier":
+			#var tile_pos = collision.collider.world_to_map($Outerciier.position)
+			#self.position = tem_start_pos
+			pass
 
 func _on_BSpring_hit_up():
 	motion.y = -bounce_height
@@ -76,11 +87,4 @@ func _on_BSpring_hit_up():
 func _on_USpring_hit_down():
 	motion.y = bounce_height
 
-func _on_LSpring_hit_left():
-	motion.x -= bounce_width
-	motion.y = -bounce_height
-
-func _on_RSpring_hit_right():
-	motion.x += bounce_width
-	motion.y = -bounce_height
 
